0828dce28c1ae3fdc67f9b17ff8ba7c2bdede4d3
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2021-11-21 19:37:16 -0800

    minor update

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Prototipe Website Ibnu Sina'
 
 export default function Layout({
   children,
